3dc234fb9437e8cb88f24c60101e51e05cfbf4fc
Mino260806 <dmezghanni@gmail.com>
2022-11-06 12:33:04 +0100

    added ability to input words

--- gamescreen.py
+++ gamescreen.py
@@ -17,12 +17,78 @@ class GameScreen(BaseScreen):
 
     def init_ui(self):
         # This frame contains all the rows
-        self.wordsframe = tk.LabelFrame(self, text="Game")
+        self.wordsframe = WordsFrame(self, self.cbridge)
+        self.wordsframe.focus_set()
+        self.wordsframe.pack()
 
+
+COLOR_GRAY = 0
+COLOR_YELLOW = 1
+COLOR_GREEN = 2
+
+
+class WordsFrame(tk.LabelFrame):
+    def __init__(self, root, cbridge):
+        super().__init__(root, text="Game")
+
+        self.root = root
+        self.cbridge = cbridge
+
+        self.current_line = 0
+        self.current_letter = 0
+        self.current_word = []
+
+        self.letter_labels = []
         # Fill the frame according to the number of lines and word length
         for row in range(LINES_COUNT):
+            self.letter_labels.append([])
+            self.grid_rowconfigure(row, minsize=60)
             for col in range(WORD_LENGTH):
-                letter = tk.Label(self.wordsframe, text="A", borderwidth=1, relief="solid", padx=20, pady=20)
-                letter.grid(row=row, column=col)
+                if row == 0:
+                    self.grid_columnconfigure(col, minsize=60)
 
-        self.wordsframe.pack()
+                letter = tk.Label(self, text="", borderwidth=1, relief="solid")
+                letter.grid(row=row, column=col, sticky="nsew")
+
+                self.letter_labels[-1].append(letter)
+
+        self.bind("<KeyPress>", self.on_key_press)
+        self.bind("<Return>", self.on_return_press)
+        self.bind("<BackSpace>", self.on_backspace_press)
+
+    def on_key_press(self, event):
+        if len(event.char) == 1 and event.char.isalpha():
+            letter = event.char
+            if self.current_letter < WORD_LENGTH:
+                letter = letter.upper()
+                self.current_word.append(letter)
+                self.current_letter_label["text"] = letter
+                self.current_letter += 1
+
+    def on_return_press(self, event):
+        if self.current_letter == WORD_LENGTH:
+            self.validate_current_line()
+
+            self.current_line += 1
+            self.current_letter = 0
+
+    def on_backspace_press(self, event):
+        if self.current_letter > 0:
+            self.current_letter -= 1
+            self.current_letter_label["text"] = ""
+
+    def validate_current_line(self):
+        word = "".join(self.current_word)
+        result = self.cbridge.interpret(word)
+        for i in range(WORD_LENGTH):
+            color = result[i]
+            if color == COLOR_GRAY:
+                self.letter_labels[self.current_line][i]["bg"] = "#aaaaaa"
+            elif color == COLOR_YELLOW:
+                self.letter_labels[self.current_line][i]["bg"] = "#ffff33"
+            elif color == COLOR_GREEN:
+                self.letter_labels[self.current_line][i]["bg"] = "#22ff55"
+
+    @property
+    def current_letter_label(self):
+        return self.letter_labels[self.current_line][self.current_letter]
